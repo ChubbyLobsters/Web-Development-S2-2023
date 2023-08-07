d3edaa8df8a43735c06078fa2131d211ec12dcf6
Cameron Yeoman (1000119549) <YEOMCJ1@student.op.ac.nz>
2023-08-08 10:47:40 +1200

    format: added lib files and imported into page for devalopment

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    import Header from "../lib/header.svelte";
+    import Footer from "../lib/footer.svelte";
+    import Sidebar from "../lib/sidebar.svelte";
+    import Nav from "../lib/nav.svelte";
+  </script>
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
