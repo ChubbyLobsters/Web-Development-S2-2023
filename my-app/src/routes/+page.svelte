75bbde6107569b213088310e2c35d6ced7a78cef
Cameron Yeoman (1000119549) <YEOMCJ1@student.op.ac.nz>
2023-08-08 10:48:54 +1200

    refactor: changed page data to get feel

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -7,5 +7,5 @@
 
 
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome</h1>
+<p>This is the begginning of my site</p>
