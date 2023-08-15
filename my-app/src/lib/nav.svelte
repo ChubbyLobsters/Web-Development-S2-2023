5a73a9bf7d125afecbcae79ebea8b41e51f95fbe
Cameron Yeoman <YEOMCJ1@student.op.ac.nz>
2023-08-16 00:34:25 +1200

    refactor: adding sizing/genral styles

--- my-app/src/lib/nav.svelte
+++ my-app/src/lib/nav.svelte
@@ -5,7 +5,14 @@
 
 
 <style>
+ .clearfix:after {visibility: hidden; display: block; font-size: 0; content: " "; clear: both; height: 0; }
 
+* { -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box;  }
+
+.innerWrapper {
+  max-width: 920px;
+  margin: 0 auto;
+}
 
 
 
