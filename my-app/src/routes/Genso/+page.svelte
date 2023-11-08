45ce5311f01e094139eaa3777de6e9bc90e4adec
Cameron Yeoman <YEOMCJ1@student.op.ac.nz>
2023-11-09 00:05:24 +1300

    feature:genso updated to generic

--- my-app/src/routes/Genso/+page.svelte
+++ my-app/src/routes/Genso/+page.svelte
@@ -1,5 +1,59 @@
 <script>
-    import Layout from '../../lib/Gallarys/layoutGenso.svelte';
-  </script>
-  
-  <Layout />
+  import Layout from '../../lib/galleryIndex.svelte';
+  let myImages = [
+  'images/genso/genso1.JPG',
+  'images/genso/genso2.JPG',
+  'images/genso/genso3.JPG',
+  'images/genso/genso4.JPG',
+  'images/genso/genso5.JPG',
+  'images/genso/genso6.JPG',
+  'images/genso/genso7.JPG',
+  'images/genso/genso8.JPG',
+  'images/genso/genso9.JPG',
+  'images/genso/genso10.JPG',
+  'images/genso/genso11.JPG',
+  'images/genso/genso12.JPG',
+  'images/genso/genso13.JPG',
+  'images/genso/genso14.JPG',
+];
+</script>
+
+<div class="profile-container">
+  <div class="center-profile">
+    <h2 class="profile-title">Gensō-tekina Kaosu 幻 想 的 な カ オ ス  </h2>
+    <h3 class="profile-blurb">
+      Fantastical Chaos
+    </h3>
+  </div>
+</div>
+
+
+<Layout images={myImages} altText="Custom Alt Text" />
+
+<style>
+  .profile-container {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   height: 40vh; /* Adjust the height as needed for your layout */
+ }
+ 
+ .center-profile {
+   text-align: center;
+   padding: 15px; /* Add space above and below the text */
+   font-family: Arial, sans-serif; /* font family */
+   width: 85%; 
+ }
+ 
+ .profile-title {
+   font-size: 36px; 
+   font-weight: bold; /* title bold */
+ }
+ 
+ .profile-blurb {
+   font-size: 18px;
+   line-height: 1.5; /* Increased line height for readability */
+ }
+ 
+ </style>
+ 
