823254bbc88534b5c7147b989a1e4b3f7608e26e
Cameron Yeoman <YEOMCJ1@student.op.ac.nz>
2023-11-09 17:07:27 +1300

    feature:cleaned up bio for main page

--- my-app/src/lib/bio.svelte
+++ my-app/src/lib/bio.svelte
@@ -1,44 +1,81 @@
 <script>
-    let bio = "In the world of artistry, we, the craftsmen, doth aspire to sway the belief that Forgotten Dreams should ne'er be cast into the abyss of oblivion; instead, they should be enshrined in memory's keep. Through our chosen medium, we labor to breathe life into the ethereal realms of our innermost thoughts, where dreams, once vivid and poignant, may have waned into shadow. We hold it true that within these forsaken dreams doth lie untapped wellsprings of inspiration, nostalgia, and untold tales, awaiting reawakening. As artists, we bear the torch of history, rekindling the embers of dreams neglected, to set the minds of today ablaze, ensuring their grace and significance persist, refusing to wither into the forgotten corners of time. And thus, we must delve deep into the recesses of our souls, summoning forth the creepy and the bizarre, for it is in the strangest and most unsettling of dreams that the truest expressions of art may be found. In embracing the eerie and the uncanny, we unearth the hidden treasures of our creative spirits, giving voice to the forgotten dreams that linger in the darkest chambers of our hearts. And lo, we discover that even the spiky and the scary can, in their own right, be loving and daring, revealing the multifaceted nature of art and the boundless realms of human emotion.";
-
-    let imageUrl = "images/main/pagecover.jpg"; // Replace with the URL of your image.
-</script>
-
-<section class="compact-section">
-    <div class="image">
-        <img src={imageUrl} alt="Image" />
+    let bio = "I am Cameron Yeoman, the enigmatic night crawler of street art and the conductor of the underground music symphony. In the moon's tender embrace, I don my cloak of mystery and step into the hidden canvas of the city's streets, where my artistic spirit awakens with every step. My presence in the community is like a whisper in the wind, a secret shared only among the nocturnal creatures of creativity, yet unknown to even my closest friends and family."
+  
+    let goal = "My ultimate ambition dances on the edge of whimsy and wonder. As the night's patron of unconventional arts, I aspire to one day craft prosthetics that are a fusion of art and function, like extensions of the human imagination. My dreams extend to the creation of enchanting body modding devices, where reality and fantasy intertwine to sculpt the human form into a canvas of self-expression."
+  
+    let signatures = "WomenEatersOffical, MrWildFire, CY, ForGottenDreams. As forever burning citys to the ground in inspiration."
+  
+    let imageUrl1 = "images/main/portrait.jpg"; // Replace with the URL of your first image.
+    let imageUrl2 = "images/main/scribble1.jpg"; // Replace with the URL of your second image.
+  </script>
+  
+  <div class="container">
+    <div class="compact-section">
+      <div class="image">
+        <img src={imageUrl1} alt="Image" />
+      </div>
+      <div class="content">
+        <p><strong>{bio}</strong></p>
+      </div>
     </div>
-    <div class="bio">
-        <p>{bio}</p>
+  
+    <div class="additional-image">
+      <img src={imageUrl2} alt="Additional Image" />
     </div>
-</section>
-
-<style>
+  
+    <div class="goal-section">
+      <div class="content">
+        <p><strong>{goal}</strong></p>
+      </div>
+    </div>
+  
+    <div class="signatures-section">
+      <div class="content">
+        <p>{signatures}</p>
+      </div>
+    </div>
+  </div>
+  
+  <style>
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+  
+    .compact-section, .goal-section, .signatures-section {
+      max-width: 800px;
+      padding: 20px;
+    }
+  
     .compact-section {
-        display: flex;
-        align-items: center;
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 20px;
+      display: flex;
     }
-
+  
     .image {
-        flex: 1;
-        margin-right: 90px;
+      flex: 1;
+      margin-right: 30px;
     }
-
+  
     img {
-        max-width: 400px;
-        max-height: 100%;
+      max-width: 400px;
+      max-height: 100%;
     }
-
-    .bio {
-        flex: 2;
+  
+    .content {
+      flex: 2;
+    }
+  
+    .additional-image {
+      margin: 20px 0;
+      text-align: center;
     }
 
     p {
-        font-size: 12px;
-        line-height: 2.4;
-        font-weight: bold;
+      font-size: 15px;
+      font-family: Arial, sans-serif;
+      line-height: 2;
+      font-weight: bold;
     }
-</style>
+  </style>
