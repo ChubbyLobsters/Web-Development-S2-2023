b57744d829b0c3285555d34d76d0bf088bbc3083
Cameron Yeoman <YEOMCJ1@student.op.ac.nz>
2023-11-10 21:49:22 +1300

    feature: nav is now responsive and using ea6 java

--- my-app/src/lib/nav.svelte
+++ my-app/src/lib/nav.svelte
@@ -1,4 +1,5 @@
 <script>
+  // JavaScript function to scroll smoothly to a section
   function scrollIntoView({ target }) {
     const el = document.querySelector(target.getAttribute('href'));
     el.scrollIntoView({
@@ -7,19 +8,22 @@
   }
 </script>
 
-<header style="height: 220px;"> 
+<header style="height: 220px;">
   <div class="navWrapper" id="home">
     <div class="clearfix">
       <nav class="mainNav clearfix">
         <nav>
-          <ul class="left-links"> 
+          <ul class="left-links">
+            <!-- Navigation links on the left side -->
             <li><a href="/">Home</a></li>
             <li><a href="/gallery-index">Gallery</a></li>
           </ul>
           <div class="logo-container">
+            <!-- Logo container in the middle -->
             <img class="logo" src="images/main/logo.jpg" alt="Logo" />
           </div>
-          <ul class="right-links"> 
+          <ul class="right-links">
+            <!-- Navigation links on the right side -->
             <li><a href="#section-3" on:click|preventDefault={scrollIntoView}>Contact</a></li>
             <li><a href="#section-2" on:click|preventDefault={scrollIntoView}>About</a></li>
           </ul>
@@ -29,16 +33,26 @@
   </div>
 </header>
 
-
-
 <style>
-  .clearfix:after {visibility: hidden; display: block; font-size: 0; content: " "; clear: both; height: 0; }
+  .clearfix:after {
+    visibility: hidden;
+    display: block;
+    font-size: 0;
+    content: " ";
+    clear: both;
+    height: 0;
+  }
 
-  * { -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; }
+  * {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
 
   .logo {
     max-width: 160px;
     margin-top: 30px;
+    transition: all 0.5s ease;
   }
 
   nav {
@@ -46,27 +60,28 @@
     margin: 0;
     width: 100%;
     height: 50px;
-    display: flex; 
-    justify-content: space-between; 
+    display: flex;
+    justify-content: space-between;
     align-items: center;
   }
 
   header nav ul {
     padding: 0;
     margin: 0;
-    background: rgb(255, 255, 255);
-    list-style: none; 
+    list-style: none;
+    display: flex;
   }
 
   header nav ul li {
     margin: 0;
-    display: inline;
     text-align: center;
+    transition: all 0.5s ease;
   }
 
   header nav ul li:hover {
     background: none;
-    transform: scale(1.2);
+    transform: scale(1.1);
+    transition: transform 1s;
   }
 
   header nav ul li a {
@@ -74,10 +89,10 @@
     padding: 15px 20px;
     color: #000000;
     margin: 0;
-    display: inline-block;
     text-transform: uppercase;
     font-family: 'Quicksand', sans-serif;
     font-size: 1rem;
+    transition: font-size 0.5s ease, padding 0.5s ease;
   }
 
   header nav ul li a:focus {
@@ -95,6 +110,21 @@
   }
 
   .right-links {
-    float: right; 
+    float: right;
+  }
+
+  @media screen and (max-width: 768px) {
+    .logo {
+      max-width: 120px;
+      margin-top: 15px;
+    }
+
+    header nav ul li {
+      padding: 10px 15px; /* Reduce padding */
+    }
+
+    header nav ul li a {
+      font-size: 0.8rem; /* Reduce font size */
+    }
   }
 </style>
