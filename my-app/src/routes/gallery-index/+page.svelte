fcbc2e7da7b3b21a0a7b1f61c2fe0bd54f27e944
Cameron Yeoman <YEOMCJ1@student.op.ac.nz>
2023-11-10 22:39:43 +1300

    feature:gallary index is now resposive

--- my-app/src/routes/gallery-index/+page.svelte
+++ my-app/src/routes/gallery-index/+page.svelte
@@ -1,26 +1,21 @@
 <script>
-
-
-  // Define an array of words with corresponding routes
-  let words = [
+  var words = [
     { text: "Elysian Echoes: Chronicles of the Fallen Seraph", route: "/Elysian", imgSrc: "images/index/one.jpg", imgHoverSrc: "images/index/oneHoverd.jpg" },
     { text: "BLOOD LUST DEATH", route: "/BLD", imgSrc: "images/index/two.jpg", imgHoverSrc: "images/index/twoHoverd.jpg" },
     { text: "幻想的なカオス (Gensō-tekina Kaosu)", route: "/Genso", imgSrc: "images/index/three.jpg", imgHoverSrc: "images/index/threeHoverd.jpg" },
     { text: "Lumina Noctis: Ethereal Rhapsodies of Psychedelic Dreamscape", route: "/Lumina", imgSrc: "images/index/four.jpg", imgHoverSrc: "images/index/fourHoverd.jpg" },
   ];
-  
-  // Store the currently hovered route
-  let currentHoverRoute = "";
 
-  // Function to handle image hover
+  var currentHoverRoute = "";
+
   function handleImageHover(route) {
     currentHoverRoute = route;
   }
 </script>
 
-<!-- Add the top image below the navigation -->
 <div class="centered-words">
-  <img class="top-image" src="images/index/top.jpg" alt="Top Image">
+  <!-- Add an image to the top center -->
+  <img src="images/index/top.jpg" alt="Top Center Image" class="top-center-image">
   <ul>
     {#each words as word (word.route)}
       <li>
@@ -32,69 +27,81 @@
   </ul>
 </div>
 
-<!-- Add the bottom image below the footer -->
 <div class="centered-images">
-  <img src="images/index/bottom.jpg" alt="Bottom Image">
+  <!-- Make the bottom image less stretched -->
+  <img src="images/index/bottom.jpg" alt="Bottom Image" class="bottom-image">
 </div>
 
-
-
 <style>
-  /* Center all content and remove the list-style dots */
   .centered-words {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 160vh;
     margin-bottom: 20px;
     margin-top: 20px;
   }
 
   .centered-words ul {
-    list-style: none; /* Remove list-style dots for the entire list */
+    list-style: none;
+    padding: 0;
   }
 
   .word {
-    transition: color 0.3s; /* Smooth transitions */
-    padding: 40px 0; /* Add 10px vertical spacing between words */
-    font-weight: bold; /* Use a bold font */
-    display: inline-block; /* Ensure spacing between words */
-    position: relative; /* Required for pseudo-element */
+    transition: color 0.3s;
+    padding: 20px 0;
+    font-weight: bold;
+    display: inline-block;
+    position: relative;
     margin: 20px;
-    color: transparent; /* Set the font color to transparent */
-    cursor: pointer; /* Change the cursor to a pointer on hover */
+    color: transparent;
+    cursor: pointer;
   }
 
   .word img {
-    max-width: 100%; /* Ensure the image fits within the container */
-    display: block; /* Remove any extra spacing around the image */
-    transition: transform 0.3s; /* Add a smooth transform transition */
+    max-width: 100%;
+    display: block;
+    transition: transform 0.3s;
   }
 
-  /* Change the image source on hover */
   .word:hover img {
-    transform: scale(1.1); /* Scale up the image on hover */
-    content: attr(data-src);
+    transform: scale(1.1);
   }
 
-  /* Adjust the image styling for both top and bottom images */
-  .centered-images img {
-    width: 40%; /* Adjust the width of the images as needed */
-    max-height: 270px;
-    margin-top: 20px; /* Add a 20px top margin to the images */
-  }
-
-  /* Center the bottom image by using flex properties */
   .centered-images {
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: center; /* Center horizontally and vertically */
+    height: 100px; /* Make it take the full height of the viewport */
+    padding-bottom: 50px;
   }
 
-  /* Push the top image down */
-  .top-image {
-    margin-bottom: 20px; /* Adjust this value as needed */
+  .centered-images img {
+    max-width: 30%; /* Adjust the maximum width */
+    max-height: auto; /* Adjust height based on the image's aspect ratio */
+  }
+
+  .top-center-image {
+    max-width: 40%;
+    padding-bottom: 30px;
+  }
+
+  @media (max-width: 768px) {
+    .centered-words {
+      padding: 20px;
+    }
+
+    .word {
+      padding: 10px 0;
+      margin: 10px;
+    }
+
+    .word img {
+      max-width: 80%;
+    }
+
+    .centered-images img {
+      max-height: 200px;
+    }
   }
 </style>
-
